5a4cd5681860f8e9493b97e32b486f7f3329d85f
Ara Sheperdigian <Ara Sheperdigian>
2017-03-22 09:03:54 -0700

    updated emailValidate

--- emailValidate.py
+++ emailValidate.py
@@ -3,46 +3,95 @@ import smtplib
 import re
 
 try:
-    #Note this library doesn't install correcty everywhere always
+    #Note/Todo: this library doesn't install correcty everywhere, always
     from dns import resolver
 except ImportError:
     pass
 
-#Todo: Build class.
+
 fromAddress = 'asheperdigian@eltec.cc'
 
-class EmailError(Exception):
+class InvalidEmailError(ValueError):
+	"""Parent class of all exceptions raised by this module."""
+	pass
+
+class EmailDeliveryError(Exception):
+    '''Alias did not exist at Domain, or we were unable to deliver the email for someother reason. '''
     #Todo: This is lame. pass in the errors and process them.
     pass
 
 class DomianError(Exception):
+    '''We were not abale to verify domain of email '''
     #Todo: This is lame. pass in the errors and process them.
     pass
 
 
+#Todo: Put this script into it's own class.	
+	
+	
 #check if the eamil is a valid email format
 def isEmailValid(email_address):
+    """ Regex is cheap, easy to find, hard to read and verify. But fuckit, lets use it. This regex is taken from  https://github.com/scottbrady91/Python-Email-Verification-Script/blob/master/src/VerifyEmailAddress.py """
     # Todo: Check out the regex over at https://github.com/syrusakbary/validate_email/blob/master/validate_email.py
-	if not re.match(r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email_address):
+    if not re.match(r'^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email_address):
+        return False
+    return True
+	
+def validate_eAddress_parts(email_address):
+    """Lets make sure we're not just taking anything from anybody now, we have rules. Albeit, They may not be the best rules."""
+    if not isinstance(email_address,(str, str)):
+		try:
+			email = email_address.decode("ascii")
+		except ValueError:
+		    return False
+				
+    split = email_address.split('@')
+    if len(split) !=2:
+	    # Make sure there is only on '@' symbol
+        return False
+    elif len(split[0]) == 0:
+	    # Don't allow empty aliases.
 		return False
-	return True
-		
+    elif len(split[0]) > 64:
+	    # Make sure alias sting is not too long. 
+		return False
+    
+	#Final check, After that wash, lets wash it again. 
+    return isEmailValid(email_address)
+				
+				
 		
 #Check to see if the domian exits
-def testMx(email_address):
+def recordsTest(email_address):
     split = email_address.split('@')
     domain = str(split[1])
     try:
-        records = resolver.query(domain, 'MX')
-        mxRecord = records[0].exchange
-        mxRecord = str(mxRecord)  
-        return mxRecord		
-    except (resolver.NoAnswer,resolver.NXDOMAIN) as error:
-        return False
+		records = resolver.query(domain, 'MX')
+		mxRecord = records[0].exchange
+		mxRecord = str(mxRecord)
+		print "MX:{0} for email:{1} ".format(mxRecord , email_address)
+		return mxRecord		
+    except (resolver.NoNameservers,resolver.NoAnswer,resolver.NXDOMAIN) as error:
+	    # Todo: Test the below. Can you connect to the server with the A/AAAA Records? Do we get the right info there? Someone else did it this way, why can't we?
+        try:
+            records = resolver.query(domain, 'MX')
+            ARecord = [(0, str(r)) for r in records]
+            ARecord = str(ARecord)
+            print "A:{0} for email:{1} ".format(ARecord , email_address)
+            return ARecord			
+        except(resolver.NoNameservers,resolver.NoAnswer,resolver.NXDOMAIN) as error:
+            try:
+                records = resolver.query(domain, 'MX')
+                AAAARecord = [(0, str(r)) for r in records]
+                AAAARecord = str(AAAARecord)
+                print "AAAA :{0} for email:{1} ".format(ARecord , email_address)
+                return AAAARecord				
+            except(resolver.NoNameservers,resolver.NoAnswer,resolver.NXDOMAIN) as error:
+                return False
 
 # try and send an email to the address in question.
 def aliasTest(mxrecord,email):
-    
+    # Will this method work with A & AAAA records?
 
 	# SMTP Conversation
     server = smtplib.SMTP(timeout=10)
@@ -69,20 +118,20 @@ def checkUserEmail(email_address):
     #sanitze the input, think wat else should we be looking for.
     email = str(email_address).replace(' ','').replace('u\ax0','').replace('\n','')
     
-    match = isEmailValid(email_address)
+    match = validate_eAddress_parts(email_address)
     if match != True:
 	    # ToDo: Kick it back to the user to correct
-        raise ValueError('Bad email syntax')
+        raise InvalidEmailError('Bad email syntax')
         return False
 
-    domainTest = testMx(email_address)
+    domainTest = recordsTest(email_address)
     if domainTest == False:
         raise DomianError("Either the domain does not exist, or there was no answer from the server")		
         return False
 
     finalTest = aliasTest(domainTest,email_address)
     if finalTest == False:
-        raise EmailError("The alias does not exist at this domain")
+        raise EmailDeliveryError("The alias does not exist at this domain")
         return False
 			
     return True
@@ -100,10 +149,13 @@ if __name__ == "__main__":
               'asshat7@gmail.com',
               'dsfdsfsdfs',
               'shep@sheperdigian.com',
-              'ara.benjamin@eltec.cc']
+              'ara.benjamin@eltec.cc',
+              'hhpeterson@evergreenhealthcare.org',
+              'DITotaan@evergreenhealthcare.org']
 
     for email in emails:
 		try:
 			assert checkUserEmail(email) == True
-		except (AssertionError,ValueError,DomianError,EmailError) as e:
+			#now send them an email
+		except (AssertionError,ValueError,DomianError,EmailDeliveryError,InvalidEmailError) as e:
 			print "Email Address: {0} \nFalied with: {1}\n".format(email,str(e))
