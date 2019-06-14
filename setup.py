3ad1f199648cd485d117a1e3414cd1cf90ef9ca2
Arabenjamin <arabenjamin@laptop.local>
2019-06-14 15:39:11 -0700

    fixed setup.py to install from requirements.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
 from setuptools import setup
-from mailer import __version__
+#from mailer import __version__
+
+__version__ ="0.1.5"
+
+with open('requirements.txt') as f:
+    reqs = f.read().splitlines()
 
 
-install_requires = open("requirements.txt").read().split('\n')
+#install_requires = open("requirements.txt").read().split('\n')
 setup(
 	name='mailer',
 	version= __version__,
+	install_requires=reqs,
 	description='Send and Recieve emails via MS Exchange',
 	author='Ara Benjamin',
 	author_email='ara.benjamin@gmail.com',
 	url= "https://github.com/arabenjamin/email-util",
 	license='Creative Commons Attribution-Noncommercial-Share Alike license',
 	packages=['mailer'],
-	install_requires=install_requires,
 	classifiers=['Programming Language :: Python :: 2.7'],
     zip_safe=False)
